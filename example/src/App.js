b6bc7d3010007a69e3f2ad6faee7ea5a9ea5a87b
Furkan <furkanerdgn99@gmail.com>
2023-04-17 19:29:10 +0300

    first commit

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,13 @@ import { ExampleComponent } from 'furkan-buttons-ui'
 import 'furkan-buttons-ui/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (<>
+  <ExampleComponent type="primary" text="Primary" />
+  <ExampleComponent type="light" text="Light" />
+  <ExampleComponent type="dark" text="Dark" />
+  <ExampleComponent type="dashed" text="Dashed" />
+  </>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const ExampleComponent = ({ text,type }) => {
+  const className = `${styles.button} ${styles[`button--${type}`]}`;
+  return <button className={className}> {text} Button</button>
 }
